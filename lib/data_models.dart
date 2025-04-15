ac8aadeddf9becd388ad9d3d0dc226636a156b69
ahstewart <ah.stewart@outlook.com>
2025-04-14 21:06:57 -0700

    finished first stab at data model

--- lib/data_models.dart
+++ lib/data_models.dart
@@ -1,3 +1,4 @@
+import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:yaml/yaml.dart';
 import 'dart:async';
@@ -10,7 +11,7 @@ part 'data_models.g.dart';
 @JsonSerializable()
 class Pipeline {
   // define top-level components of the model pipeline configuration file
-  final List<MetaData> metadata;
+  final List<ModelMetadata> metadata;
   final List<IO> inputs;
   final List<IO> outputs;
   final List<ProcessingBlock> preprocessing;
@@ -33,7 +34,7 @@ class Pipeline {
 
 // metadata component data model
 @JsonSerializable()
-class Metadata {
+class ModelMetadata {
   // metadata fields in YAML
   @JsonKey(required: true)
   final String schema_version;
@@ -48,7 +49,7 @@ class Metadata {
   @JsonKey(required: true)
   final String source_repository;
 
-  Metadata({
+  ModelMetadata({
     required this.schema_version,
     required this.model_name,
     required this.model_version,
@@ -57,13 +58,82 @@ class Metadata {
     required this.source_repository,
   })
 
-  factory Metadata.fromJson(Map<String, dynamic> json) => _$Metadata(json);
-  Map<String, dynamic> toJson() => _$Metadata(this);
+  factory ModelMetadata.fromJson(Map<String, dynamic> json) => _$ModelMetadataFromJson(json);
+  Map<String, dynamic> toJson() => _$ModelMetadataToJson(this);
 }
 
 
+// IO component data model
+@JsonSerializable()
+class IO {
+  // input/output fields in YAML
+  @JsonKey(required: true)
+  final String name;
+  @JsonKey(defaultValue: '')
+  final String shape;
+  @JsonKey(required: true)
+  final String dtype;
+  @JsonKey(defaultValue: '')
+  final String description;
+
+  IO({
+    required this.name,
+    required this.shape,
+    required this.dtype,
+    required this.description,
+  })
+
+  factory IO.fromJson(Map<String, dynamic> json) => _$IOFromJson(json);
+  Map<String, dynamic> toJson() => _$IOToJson(this);
+}
 
 
+// processing block component data model
+@JsonSerializable()
+class ProcessingBlock {
+  // processing block fields in YAML
+  @JsonKey(defaultValue: '')
+  final String input_name;
+  @JsonKey(defaultValue: '')
+  final String output_name;
+  @JsonKey(defaultValue: '')
+  final String expects_type;
+  @JsonKey(defaultValue: '')
+  final String interpretation;
+  @JsonKey(required: true)
+  final List<String> source_tensors;
+  final List<ProcessingStep> steps;
+
+  ProcessingBlock({
+    required this.input_name,
+    required this.output_name,
+    required this.expects_type,
+    required this.interpretation,
+    required this.source_tensors,
+    required this.steps,
+  })
+
+  factory ProcessingBlock.fromJson(Map<String, dynamic> json) => _$ProcessingBlockFromJson(json);
+  Map<String, dynamic> toJson() => _$ProcessingBlockToJson(this);
+}
+
+
+// processing step component data model
+@JsonSerializable()
+class ProcessingStep {
+  // processing step fields in YAML
+  @JsonKey(required: true)
+  final String step;
+  final Map<String, dynamic> params;
+
+  ProcessingStep({
+    required this.step,
+    required this.params,
+  })
+
+  factory ProcessingStep.fromJson(Map<String, dynamic> json) => _$ProcessingStepFromJson(json);
+  Map<String, dynamic> toJson() => _$ProcessingStepToJson(this);
+}
 
 
 Future<Map> parseMetadata(String metadataPath) async {
